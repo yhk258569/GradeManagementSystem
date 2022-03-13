b72c5a3b96e29d66705cb1149bd878723ec8c9ce
yhk258569 <yhk258569@naver.com>
2022-03-13 15:45:52 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Grades");
+		System.out.println("2. Delete Grades");
+		System.out.println("3. Edit Grades");
+		System.out.println("4. View Grades");
+		System.out.println("5. Exit");
+	}
+
+}
